160045269ccf358be3bdbb72b3c90aa5a1181399
AleksandraKasprzak <275182@student.pwr.edu.pl>
2026-05-16 16:52:48 +0200

    Update file paths for x1.txt and y1.txt

--- OLA/OLA_K/dopasowania_dla_danych_bez_IX.py
+++ OLA/OLA_K/dopasowania_dla_danych_bez_IX.py
@@ -20,10 +20,10 @@ def triple_lorentzian(x,
     )
 
 
-with open("/workspaces/Ola_sie_wkurwi_jak_nie_bedzie_5/OLA/x1.txt", "r") as fx:
+with open("/workspaces/analiza/OLA/x1.txt", "r") as fx:
     x_data = np.array([float(line.strip()) for line in fx])
 
-with open("/workspaces/Ola_sie_wkurwi_jak_nie_bedzie_5/OLA/y1.txt", "r") as fy:
+with open("/workspaces/analiza/OLA/y1.txt", "r") as fy:
     y_data = np.array([float(line.strip()) for line in fy])
 
 
