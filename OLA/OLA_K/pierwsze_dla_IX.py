aa8001ced34e85f338fcec51cce840d976b74bf4
AleksandraKasprzak <275182@student.pwr.edu.pl>
2026-05-16 16:54:05 +0200

    Update pierwsze_dla_IX.py

--- OLA/OLA_K/pierwsze_dla_IX.py
+++ OLA/OLA_K/pierwsze_dla_IX.py
@@ -21,10 +21,10 @@ def triple_lorentzian(x,
     )
 
 
-with open("/workspaces/Ola_sie_wkurwi_jak_nie_bedzie_5/OLA/x3.txt", "r") as fx:
+with open("/workspaces/analiza/OLA/x3.txt", "r") as fx:
     x_data = np.array([float(line.strip()) for line in fx])
 
-with open("/workspaces/Ola_sie_wkurwi_jak_nie_bedzie_5/OLA/y3.txt", "r") as fy:
+with open("/workspaces/analiza/OLA/y3.txt", "r") as fy:
     y_data = np.array([float(line.strip()) for line in fy])
 
 
